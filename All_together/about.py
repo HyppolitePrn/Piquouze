c7e620291bd03de5477835f09059a6880fcb17df
JbMig <jbmigone@gmail.com>
2021-12-21 04:13:33 +0100

    Retour sur le menu start possible

--- All_together/about.py
+++ All_together/about.py
@@ -1,6 +1,7 @@
 from time import sleep
 from os import system
 from Mac_or_wind import os
+from liste import start
 
 def info():
     if os == "mac":
@@ -10,8 +11,8 @@ def info():
     print("Toute ressemblance avec un quelconque fait réel est fortuite")
     sleep(1.5)
     print("Tous les événements ne sont que pure fiction")
-    sleep(1.5)
-    print("Projet RPG Hetic W1 réalisé par:")
+    sleep(2)
+    print("\nProjet RPG Hetic W1 réalisé par:")
     sleep(0.5)
     print("Abdoulaye Diallo")
     sleep(0.5)
@@ -20,5 +21,13 @@ def info():
     print("Hyppolite Pernot")
     sleep(0.5)
     print("Jean-Baptiste Migone")
-    sleep(0.5)
+    sleep(3)
+    if os == "mac":
+        from menu_nav_mac import menu_nav_mac
+        system('clear')
+        menu_nav_mac(start())
+    elif os == "windows":
+        from Menu_nav import menu_nav
+        system('cls')
+        menu_nav(start())
     return
